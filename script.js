636730b50117504252b9bf5eb3de094aae4d2ca0
Sofia Malakaeva <malakaevasofia@gmail.com>
2024-10-29 12:48:41 +0300

    сердевчкл

---
+++ script.js
@@ -0,0 +1,9 @@
+button.onclick = function () {
+  if (button.style.backgroundColor == 'black') {
+    button.style.backgroundColor = 'white';
+    button.style.color = 'black';
+} else {
+    button.style.backgroundColor = 'black';
+    button.style.color = 'white';
+  }
+}
